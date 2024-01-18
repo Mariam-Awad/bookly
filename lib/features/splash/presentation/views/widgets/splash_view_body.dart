7bb78b7f35dbbf971f39d46a8b4dab0bda23f2d3
Mariam-Awad <62141557+Mariam-Awad@users.noreply.github.com>
2024-01-18 12:19:15 +0200

    add text in splash& dark mode in main

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -10,7 +10,18 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       // .stretch it is mean the childrens take the largest width they can --> the screen width
-      children: [Image.asset(AssetManager.logo)],
+      children: [
+        Image.asset(AssetManager.logo),
+        const SizedBox(
+          height: 4.0,
+        ),
+        const Text(
+          'Read Free Books',
+          textAlign: TextAlign.center,
+          // al text widget wa5da full width beta3 al screen
+          // lama 3mlt textAlign.center b2t fe al screen center
+        )
+      ],
     );
   }
 }
