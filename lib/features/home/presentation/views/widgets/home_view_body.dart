eb99e401fa3167a2d29f4fc70feb76858610426b
Mariam-Awad <62141557+Mariam-Awad@users.noreply.github.com>
2024-01-28 11:55:32 +0200

    Best Seller Item Part 2

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets_manager.dart';
 import 'package:bookly/core/utils/styles_manager.dart';
 import 'package:bookly/features/home/presentation/views/widgets/books_list_view.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
@@ -8,20 +9,67 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomeAppBar(),
-        BooksListView(),
-        SizedBox(
-          height: 50.0,
-        ),
-        Text(
-          'Best Seller',
-          textAlign: TextAlign.left,
-          style: StylesManager.titleMedium,
-        ),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomeAppBar(),
+          BooksListView(),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            'Best Seller',
+            textAlign: TextAlign.left,
+            style: StylesManager.textStyle18,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: const DecorationImage(
+                      image: AssetImage(AssetManager.logo), fit: BoxFit.fill)),
+            ),
+          ),
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: const Text(
+                    'Harry Potter and the Goblet of the Fire',
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    style: StylesManager.textStyle20,
+                  )),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
